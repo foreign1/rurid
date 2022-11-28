d048b1bc4140b109e50e60d5ad82b068f167ad39
foreign1 <32528727+foreign1@users.noreply.github.com>
2022-11-28 15:08:20 +0100

    bump erroying lint-rule

--- src/rurid.ts
+++ src/rurid.ts
@@ -1,11 +1,11 @@
 import generateID from './utilities';
 import languageConstructs from './languageConstructs';
 
-/**
+/*
  * @param {String} [lang] expected language (default "en")
  * @param {string[]} [separators=['-', '.', '_']] - list of acceptable separators between words (default ['-', '.', '_'])
  * @return {String} generated human readable identifier
- * */
+ */
 
 const rurid = (lang: string = 'en', separators: string[] = ['-', '.', '_']): string => {
   const adjectives = languageConstructs[lang]['adjectives'];
